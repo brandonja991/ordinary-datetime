599796c5a828b79e19cacbaa19c7e34b5f74117f
Brandon Jackson <brandonja991@gmail.com>
2023-02-26 12:43:37 -0600

    Add TimeModifiers and testing

---
+++ src/TimeModifiers.php
@@ -0,0 +1,136 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ordinary\DateTime;
+
+trait TimeModifiers
+{
+    use TimePartGetters;
+
+    abstract public function overrideTime(
+        ?int $hour = null,
+        ?int $minute = null,
+        ?int $second = null,
+        ?int $microsecond = null,
+    ): DateTimeImmutable;
+
+    abstract public function overrideDate(?int $year = null, ?int $month = null, ?int $day = null): DateTimeImmutable;
+
+    abstract public function assertModify(string $modifier): DateTimeImmutable;
+
+    public function startOfSecond(): DateTimeImmutable
+    {
+        return $this->overrideTime(
+            microsecond: 0,
+        );
+    }
+
+    public function startOfMinute(): DateTimeImmutable
+    {
+        return $this->overrideTime(
+            second: 0,
+            microsecond: 0,
+        );
+    }
+
+    public function startOfHour(): DateTimeImmutable
+    {
+        return $this->overrideTime(
+            minute: 0,
+            second: 0,
+            microsecond: 0,
+        );
+    }
+
+    public function startOfDay(): DateTimeImmutable
+    {
+        return $this->overrideTime(0, 0, 0, 0);
+    }
+
+    public function startOfWeek(DayOfWeek $startOfWeek = DayOfWeek::Sunday): DateTimeImmutable
+    {
+        $new = $this->dayOfWeekEnum() !== $startOfWeek
+            ? $this->assertModify('last ' . $startOfWeek->fullName())
+            : $this;
+
+        return $new->startOfDay();
+    }
+
+    public function startOfMonth(): DateTimeImmutable
+    {
+        return $this->overrideDate(day: 1)->startOfDay();
+    }
+
+    public function startOfQuarter(): DateTimeImmutable
+    {
+        return $this->overrideDate(
+            month: $this->monthEnum()->startOfQuarter()->value,
+            day: 1,
+        )->startOfDay();
+    }
+
+    public function startOfSemester(): DateTimeImmutable
+    {
+        return $this->overrideDate(
+            month: $this->monthEnum()->startOfSemester()->value,
+            day: 1,
+        )->startOfDay();
+    }
+
+    public function startOfYear(): DateTimeImmutable
+    {
+        return $this->overrideDate(month: 1, day: 1)->startOfDay();
+    }
+
+    public function endOfSecond(): DateTimeImmutable
+    {
+        return $this->overrideTime(microsecond: TimeUnit::Microsecond->max());
+    }
+
+    public function endOfMinute(): DateTimeImmutable
+    {
+        return $this->overrideTime(second: TimeUnit::Second->max())->endOfSecond();
+    }
+
+    public function endOfHour(): DateTimeImmutable
+    {
+        return $this->overrideTime(minute: TimeUnit::Minute->max())->endOfMinute();
+    }
+
+    public function endOfDay(): DateTimeImmutable
+    {
+        return $this->overrideTime(TimeUnit::Hour->max())->endOfHour();
+    }
+
+    public function endOfWeek(DayOfWeek $startOfWeek = DayOfWeek::Sunday): DateTimeImmutable
+    {
+        $endOfWeek = $startOfWeek->yesterday();
+
+        $new = $this->dayOfWeekEnum() !== $endOfWeek ? $this->assertModify($endOfWeek->fullName()) : $this;
+
+        return $new->endOfDay();
+    }
+
+    public function endOfMonth(): DateTimeImmutable
+    {
+        return $this->overrideDate(
+            day: $this->monthEnum()->days($this->leapYear()),
+        )->endOfDay();
+    }
+
+    public function endOfQuarter(): DateTimeImmutable
+    {
+        return $this->overrideDate(month: $this->monthEnum()->endOfQuarter()->value)->endOfMonth();
+    }
+
+    public function endOfSemester(): DateTimeImmutable
+    {
+        return $this->overrideDate(month: $this->monthEnum()->endOfSemester()->value)->endOfMonth();
+    }
+
+    public function endOfYear(): DateTimeImmutable
+    {
+        return $this->overrideDate(month: 12)->endOfMonth();
+    }
+}
---
+++ tests/TimeModifiersTest.php
@@ -0,0 +1,261 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ordinary\DateTime;
+
+use Generator;
+use PHPUnit\Framework\TestCase;
+
+class TimeModifiersTest extends TestCase
+{
+    public static function monthProvider(): Generator
+    {
+        foreach (Month::cases() as $month) {
+            yield [$month];
+        }
+    }
+
+    public static function weekProvider(): Generator
+    {
+        $datesToTest = [
+            '2023-02-25',
+            '2020-02-29',
+            '2019-01-01',
+            '2017-12-31',
+        ];
+
+        foreach ($datesToTest as $date) {
+            // test each date with each day of week as start day
+            $dateObject = new DateTimeImmutable($date);
+
+            foreach (DayOfWeek::cases() as $dayOfWeek) {
+                yield [$dateObject, $dayOfWeek];
+            }
+        }
+    }
+
+    public static function assertEndOfSecond(DateTimeImmutable $dateTime): void
+    {
+        self::assertSame(TimeUnit::Microsecond->max(), $dateTime->microsecond());
+    }
+
+    public static function assertEndOfMinute(DateTimeImmutable $dateTime): void
+    {
+        self::assertSame(TimeUnit::Second->max(), $dateTime->second());
+        self::assertEndOfSecond($dateTime);
+    }
+
+    public static function assertEndOfHour(DateTimeImmutable $dateTime): void
+    {
+        self::assertSame(TimeUnit::Minute->max(), $dateTime->minute());
+        self::assertEndOfMinute($dateTime);
+    }
+
+    public static function assertEndOfDay(DateTimeImmutable $dateTime): void
+    {
+        self::assertSame(TimeUnit::Hour->max(), $dateTime->hour());
+        self::assertEndOfHour($dateTime);
+    }
+
+    public static function assertEndOfWeek(DateTimeImmutable $dateTime, DayOfWeek $starOfWeek): void
+    {
+        $endOfWeek = $starOfWeek->yesterday();
+
+        self::assertSame($endOfWeek, $dateTime->dayOfWeekEnum());
+        self::assertEndOfDay($dateTime);
+    }
+
+    public static function assertEndOfMonth(DateTimeImmutable $dateTime): void
+    {
+        self::assertSame($dateTime->monthEnum()->days($dateTime->leapYear()), $dateTime->day());
+        self::assertEndOfDay($dateTime);
+    }
+
+    public static function assertEndOfQuarter(DateTimeImmutable $dateTime): void
+    {
+        self::assertSame($dateTime->monthEnum()->endOfQuarter(), $dateTime->monthEnum());
+        self::assertEndOfMonth($dateTime);
+    }
+
+    public static function assertEndOfSemester(DateTimeImmutable $dateTime): void
+    {
+        self::assertSame($dateTime->monthEnum()->endOfSemester(), $dateTime->monthEnum());
+        self::assertEndOfMonth($dateTime);
+    }
+
+    public static function assertEndOfYear(DateTimeImmutable $dateTime): void
+    {
+        self::assertSame(12, $dateTime->month());
+        self::assertEndOfMonth($dateTime);
+    }
+
+    public static function assertStartOfSecond(DateTimeImmutable $dateTime): void
+    {
+        self::assertSame(0, $dateTime->microsecond());
+    }
+
+    public static function assertStartOfMinute(DateTimeImmutable $dateTime): void
+    {
+        self::assertSame(0, $dateTime->second());
+        self::assertStartOfSecond($dateTime);
+    }
+
+    public static function assertStartOfHour(DateTimeImmutable $dateTime): void
+    {
+        self::assertSame(0, $dateTime->minute());
+        self::assertStartOfMinute($dateTime);
+    }
+
+    public static function assertStartOfDay(DateTimeImmutable $dateTime): void
+    {
+        self::assertSame(0, $dateTime->hour());
+        self::assertStartOfHour($dateTime);
+    }
+
+    public static function assertStartOfWeek(DateTimeImmutable $dateTime, DayOfWeek $starOfWeek): void
+    {
+        self::assertSame($starOfWeek, $dateTime->dayOfWeekEnum());
+        self::assertStartOfDay($dateTime);
+    }
+
+    public static function assertStartOfMonth(DateTimeImmutable $dateTime): void
+    {
+        self::assertSame(1, $dateTime->day());
+        self::assertStartOfDay($dateTime);
+    }
+
+    public static function assertStartOfQuarter(DateTimeImmutable $dateTime): void
+    {
+        self::assertSame($dateTime->monthEnum()->startOfQuarter(), $dateTime->monthEnum());
+        self::assertStartOfMonth($dateTime);
+    }
+
+    public static function assertStartOfSemester(DateTimeImmutable $dateTime): void
+    {
+        self::assertSame($dateTime->monthEnum()->startOfSemester(), $dateTime->monthEnum());
+        self::assertStartOfMonth($dateTime);
+    }
+
+    public static function assertStartOfYear(DateTimeImmutable $dateTime): void
+    {
+        self::assertSame(1, $dateTime->month());
+        self::assertStartOfMonth($dateTime);
+    }
+
+    public function testEndOfSecond(): void
+    {
+        self::assertEndOfSecond((new DateTimeImmutable())->endOfSecond());
+    }
+
+    public function testEndOfMinute(): void
+    {
+        self::assertEndOfMinute((new DateTimeImmutable())->endOfMinute());
+    }
+
+    public function testEndOfHour(): void
+    {
+        self::assertEndOfHour((new DateTimeImmutable())->endOfHour());
+    }
+
+    public function testEndOfDay(): void
+    {
+        self::assertEndOfDay((new DateTimeImmutable())->endOfDay());
+    }
+
+    /** @dataProvider weekProvider */
+    public function testEndOfWeek(DateTimeImmutable $dateTime, DayOfWeek $startOfWeek): void
+    {
+        $test = $dateTime->endOfWeek($startOfWeek);
+
+        self::assertEndOfWeek($test, $startOfWeek);
+
+
+        $endOfWeek = $startOfWeek->yesterday();
+        $daysUntil = $dateTime->dayOfWeekEnum()->daysUntil($endOfWeek);
+
+        self::assertEquals($dateTime->assertModify('+' . $daysUntil . ' days')->endOfDay(), $test);
+    }
+
+    /** @dataProvider monthProvider */
+    public function testEndOfMonth(Month $month): void
+    {
+        self::assertEndOfMonth((new DateTimeImmutable())->overrideDate(month: $month->value)->endOfMonth());
+    }
+
+    /** @dataProvider monthProvider */
+    public function testEndOfQuarter(Month $month): void
+    {
+        $dateTime = (new DateTimeImmutable())->overrideDate(month: $month->value)->endOfQuarter();
+        self::assertEndOfQuarter($dateTime);
+    }
+
+    /** @dataProvider monthProvider */
+    public function testEndOfSemester(Month $month): void
+    {
+        $dateTime = (new DateTimeImmutable())->overrideDate(month: $month->value)->endOfSemester();
+        self::assertEndOfSemester($dateTime);
+    }
+
+    public function testEndOfYear(): void
+    {
+        self::assertEndOfYear((new DateTimeImmutable())->endOfYear());
+    }
+
+    public function testStartOfSecond(): void
+    {
+        self::assertStartOfSecond((new DateTimeImmutable())->startOfSecond());
+    }
+
+    public function testStartOfMinute(): void
+    {
+        self::assertStartOfMinute((new DateTimeImmutable())->startOfMinute());
+    }
+
+    public function testStartOfHour(): void
+    {
+        self::assertStartOfHour((new DateTimeImmutable())->startOfHour());
+    }
+
+    public function testStartOfDay(): void
+    {
+        self::assertStartOfDay((new DateTimeImmutable())->startOfDay());
+    }
+
+    /** @dataProvider weekProvider */
+    public function testStartOfWeek(DateTimeImmutable $dateTime, DayOfWeek $startOfWeek): void
+    {
+        $test = $dateTime->startOfWeek($startOfWeek);
+
+        self::assertStartOfWeek($test, $startOfWeek);
+
+        $daysSince = $dateTime->dayOfWeekEnum()->daysSince($startOfWeek);
+
+        self::assertEquals($dateTime->assertModify('-' . $daysSince . ' days'), $test);
+    }
+
+    /** @dataProvider monthProvider */
+    public function testStartOfMonth(Month $month): void
+    {
+        self::assertStartOfMonth((new DateTimeImmutable())->overrideDate(month: $month->value)->startOfMonth());
+    }
+
+    /** @dataProvider monthProvider */
+    public function testStartOfQuarter(Month $month): void
+    {
+        $dateTime = (new DateTimeImmutable())->overrideDate(month: $month->value)->startOfQuarter();
+        self::assertStartOfQuarter($dateTime);
+    }
+
+    /** @dataProvider monthProvider */
+    public function testStartOfSemester(Month $month): void
+    {
+        $dateTime = (new DateTimeImmutable())->overrideDate(month: $month->value)->startOfSemester();
+        self::assertStartOfSemester($dateTime);
+    }
+
+    public function testStartOfYear(): void
+    {
+        self::assertStartOfYear((new DateTimeImmutable())->startOfYear());
+    }
+}
