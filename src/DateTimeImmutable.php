574001820758abeeaea7fadfbb0fdada74cc8567
Brandon Jackson <brandonja991@gmail.com>
2023-02-26 14:39:48 -0600

    Implement DateTimeImmutable and tests

--- src/DateTimeImmutable.php
+++ src/DateTimeImmutable.php
@@ -4,17 +4,123 @@ declare(strict_types=1);
 
 namespace Ordinary\DateTime;
 
-use DateTimeImmutable as PHPDateTimeImmutableAlias;
+use DateTimeImmutable as PHPDateTimeImmutable;
+use DateTimeInterface;
+use Exception;
 
-class DateTimeImmutable extends PHPDateTimeImmutableAlias
+class DateTimeImmutable extends PHPDateTimeImmutable
 {
-    /*
-     * override to define param with correct type - work around due to psalm in phpstorm
+    use OrdinalTimeComparison;
+    use TimePartGetters;
+    use TimeModifiers;
+    use RoundedTimeComparison;
+
+    public static function assertSelf(DateTimeInterface $dateTime): self
+    {
+        /** @var self $result not returned directly to work around createTimeInterface result not being static */
+        $result =  $dateTime instanceof self
+            ? $dateTime
+            : self::createFromInterface($dateTime);
+
+        return $result;
+    }
+
+    /**
+     * @param array<string, mixed> $data
+     *
+     * note: override to define param with correct type - work around due to psalm in phpstorm
      * detecting param 1 type mismatch
      */
-    /** @param array<string, mixed> $data */
     public function __unserialize(array $data): void
     {
         parent::__unserialize($data);
+    }
+
+    public function overrideTime(
+        ?int $hour = null,
+        ?int $minute = null,
+        ?int $second = null,
+        ?int $microsecond = null,
+    ): self {
+        if ($hour) {
+            TimeUnit::Hour->validate($hour);
+        }
+
+        if ($minute) {
+            TimeUnit::Minute->validate($minute);
+        }
+
+        if ($second) {
+            TimeUnit::Second->validate($second);
+        }
+
+        if ($microsecond) {
+            TimeUnit::Microsecond->validate($microsecond);
+        }
+
+        return $this->setTime(
+            $hour ?? $this->hour(),
+            $minute ?? $this->minute(),
+            $second ?? $this->second(),
+            $microsecond ?? $this->microsecond(),
+        );
+    }
+
+    public function overrideDate(?int $year = null, ?int $month = null, ?int $day = null): self
+    {
+        if ([$year, $month, $day] === [null, null, null]) {
+            // short circuit if no changes
+            return $this;
+        }
+
+        $effectiveYear = $year ?? $this->year();
+        $effectiveMonth = $month ?? $this->month();
+        $effectiveDay = $day ?? $this->day();
+
+        if ($year !== null) {
+            assert($year >= 0, new UnexpectedValueException('Unexpected negative year'));
+            assert($year <= 9999, new UnexpectedValueException('Unexpected year greater than 9999'));
+        }
+
+        if ($month !== null) {
+            $monthVO = Month::tryFrom($month)
+                ?? throw new UnexpectedValueException('Invalid month: ' . $month);
+        }
+
+        $monthVO ??= $this->monthEnum();
+        // always validate the day since a change to year, month, or day can invalidate the day
+        $monthVO->validateDay($effectiveDay, (new DateTimeImmutable("$effectiveYear-01-01"))->leapYear());
+
+        return $this->setDate(
+            $effectiveYear,
+            $effectiveMonth,
+            $effectiveDay,
+        );
+    }
+
+    public function assertModify(string $modifier): self
+    {
+        try {
+            $new = $this->modify($modifier);
+        } catch (Exception $e) {
+            throw new UnexpectedValueException(
+                'Failed to modify date time using modifier: ' . $modifier,
+                previous: $e,
+            );
+        }
+
+        assert(
+            $new instanceof self,
+            new UnexpectedValueException(
+                'Failed to modify date time using modifier: ' . $modifier,
+            ),
+        );
+
+        return $new;
+    }
+
+    private function formatInt(string $format): int
+    {
+        return (int) $this->format($format);
     }
 }
---
+++ tests/DateTimeImmutableTest.php
@@ -0,0 +1,145 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ordinary\DateTime;
+
+use DateTime;
+use DateTimeImmutable as PHPDateTimeImmutableAlias;
+use Exception;
+use Generator;
+use PHPUnit\Framework\TestCase;
+
+class DateTimeImmutableTest extends TestCase
+{
+    public static function overrideTimeProvider(): Generator
+    {
+        yield [0, 0, 0, 0, null]; // valid lower bounds
+
+        yield [23, 59, 59, 999_999, null]; // valid upper bounds
+
+        yield [0, 0, 0, 1_000_000, UnexpectedValueException::class]; // over bound microsecond
+        yield [0, 0, 0, -1, UnexpectedValueException::class]; // under bound microsecond
+
+        yield [0, 0, 60, 0, UnexpectedValueException::class]; // over bound second
+        yield [0, 0, -1, 0, UnexpectedValueException::class]; // under bound second
+
+        yield [0, 60, 0, 0, UnexpectedValueException::class]; // over bound minute
+        yield [0, -1, 0, 0, UnexpectedValueException::class]; // under bound minute
+
+        yield [24, 0, 0, 0, UnexpectedValueException::class]; // over bound hour
+        yield [-1, 0, 0, 0, UnexpectedValueException::class]; // under bound hour
+    }
+
+    public static function overrideDateProvider(): Generator
+    {
+        foreach (Month::cases() as $month) {
+            yield [0, $month->value, 1, null]; // valid month lower bounds
+            yield [9999, $month->value, $month->days(), null]; // valid month upper bounds
+            yield [2020, $month->value, $month->days(true), null]; // valid month upper bounds leap year
+
+            yield [-1, $month->value, 1, UnexpectedValueException::class]; // invalid year lower bound
+            yield [10_000, $month->value, 1, UnexpectedValueException::class]; // invalid year upper bound
+
+            yield [0, $month->value, 0, UnexpectedValueException::class]; // invalid day lower bound
+            yield [9999, $month->value, $month->days() + 1, UnexpectedValueException::class]; // invalid day upper bound
+            yield [
+                2020,
+                $month->value,
+                $month->days(true) + 1,
+                UnexpectedValueException::class,
+            ]; // invalid day upper bound leap year
+        }
+
+        yield [0, 0, 1, UnexpectedValueException::class]; // invalid month lower bound
+        yield [0, 13, 1, UnexpectedValueException::class]; // invalid month upper bound
+    }
+
+    public static function assertModifyProvider(): Generator
+    {
+        yield ['1 day ago', null];
+        yield ['foobarbaz', UnexpectedValueException::class];
+    }
+
+    /**
+     * @param ?class-string<Exception> $exception
+     * @dataProvider  assertModifyProvider
+     */
+    public function testAssertModify(string $modifier, ?string $exception): void
+    {
+        $og = new DateTimeImmutable();
+
+        if ($exception) {
+            self::expectException($exception);
+        }
+
+        $test = $og->assertModify($modifier);
+
+        self::assertInstanceOf(DateTimeImmutable::class, $test);
+    }
+
+    public function testAssertSelf(): void
+    {
+        $test1 = new DateTimeImmutable();
+
+        self::assertSame($test1, DateTimeImmutable::assertSelf($test1));
+
+        $test2 = new DateTime();
+        $result2 = DateTimeImmutable::assertSelf($test2);
+
+        self::assertNotSame($test2, $result2);
+        self::assertEquals($test2, $result2);
+        self::assertInstanceOf(DateTimeImmutable::class, $result2);
+
+        $test3 = new PHPDateTimeImmutableAlias();
+        $result3 = DateTimeImmutable::assertSelf($test3);
+
+        self::assertNotSame($test3, $result3);
+        self::assertEquals($test3, $result3);
+        self::assertInstanceOf(DateTimeImmutable::class, $result3);
+    }
+
+    /**
+     * @param ?class-string<Exception> $exception
+     * @dataProvider overrideDateProvider
+     */
+    public function testOverrideDate(?int $year, ?int $month, ?int $day, ?string $exception): void
+    {
+        $og = new DateTimeImmutable();
+
+        if ($exception) {
+            self::expectException($exception);
+        }
+
+        $test = $og->overrideDate($year, $month, $day);
+
+        self::assertSame($year ?? $og->year(), $test->year());
+        self::assertSame($month ?? $og->month(), $test->month());
+        self::assertSame($day ?? $og->day(), $test->day());
+    }
+
+    /**
+     * @param ?class-string<Exception> $exception
+     * @dataProvider overrideTimeProvider
+     */
+    public function testOverrideTime(
+        ?int $hour,
+        ?int $minute,
+        ?int $second,
+        ?int $microsecond,
+        ?string $exception,
+    ): void {
+        $og = new DateTimeImmutable();
+
+        if ($exception) {
+            self::expectException($exception);
+        }
+
+        $test = $og->overrideTime($hour, $minute, $second, $microsecond);
+
+        self::assertSame($hour ?? $og->hour(), $test->hour());
+        self::assertSame($minute ?? $og->minute(), $test->minute());
+        self::assertSame($second ?? $og->second(), $test->second());
+        self::assertSame($microsecond ?? $og->microsecond(), $test->microsecond());
+    }
+}
